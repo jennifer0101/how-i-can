60d808b362b2eef101bc90dbb39d49869448dc8b
Jennifer Faye <jennifer@fayecreative.com>
2021-03-14 18:21:59 -0500

    onclick works to alert

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,16 @@
-function clearText() {
-    document.getElementById("clear") = '';
-  }
+function push(element) 
+{
+    alert("clicked on " + element.value);
+}
 
+
+
+// function evaluateText(input){
+//     let str = String(input);
+//     for( let i = 0; i < str.length; i++){
+//               console.log(str.charAt(i));
+//         if(!isNaN(str.charAt(i))){           //if the string is a number, do the following
+//             return true;
+//         }
+//     }
+// }
