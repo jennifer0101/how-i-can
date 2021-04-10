3eac709070960587922b9a8a2c834243eee7a176
Jennifer Faye <jennifer@fayecreative.com>
2021-04-09 19:51:59 -0500

    adding items to list when button clicked

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -5,15 +5,29 @@ function clearText(){
 
 // function to evaluate text field and display results
 function evalFunction(){
-    let x = document.getElementById("textArea").value;
-    console.log(x);
-    for (let i=0; i < x.length; i++) {
-        console.log('number', x[i]);    
-    } if(isNaN(x)) {
-        document.getElementById("demo").innerHTML =("The result is text."); 
-    } else {
-        document.getElementById("demo").innerHTML =("The result is a number.");
-    }
-  }
+    // let x = document.getElementById("textArea").value;
+    // console.log(x);
+    let ul = document.getElementById("resultsList");
+    let listString = document.getElementById("textArea");
+    let li = document.createElement("li");
+    li.setAttribute('id', listString.value);
+    li.appendChild(document.createTextNode(listString.value));
+    ul.appendChild(li);   
+}
+
+
+    
+
+
+
+
+    // for (let i=0; i < x.length; i++) {
+    //     console.log('number', x[i]);    
+    // } if(isNaN(x)) {
+    //     document.getElementById("demo").innerHTML =("The result is text."); 
+    // } else {
+    //     document.getElementById("demo").innerHTML =("The result is a number.");
+    // }
+//   }
 
 
